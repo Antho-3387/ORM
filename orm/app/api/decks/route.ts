049440d7399e9e4e9ed38ad868475e6b2a729f17
root <root@DESKTOP-DMGUUFL.localdomain>
2026-04-01 21:19:22 +0200

    add required type field for Card

--- orm/app/api/decks/route.ts
+++ orm/app/api/decks/route.ts
@@ -88,6 +88,7 @@ export async function POST(request: NextRequest) {
                 data: {
                   name: cardName,
                   scryfallId: '',
+                  type: 'Unknown',
                 },
               })
             }
