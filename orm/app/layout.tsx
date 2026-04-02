e8d0f5dff2b89d82b3e6eda588f00f3fc1c66876
Anthony <root@PCdeHash.localdomain>
2026-04-02 20:13:34 +0200

    Trigger clean rebuild on Render

--- orm/app/layout.tsx
+++ orm/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Magic Decks",
+  title: "Magic Decks ORM",
   description: "Manage your Magic: The Gathering deck collection",
 };
 
