5c84fbb6a1a473a33c630e4c36c6675384601c38
Anthony <root@PCdeHash.localdomain>
2026-04-02 18:29:22 +0200

    Add automatic Prisma migration to Render build process

--- orm/migrate.sh
+++ orm/migrate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export DATABASE_URL='postgresql://postgres:G9%23fL7mZ2!qXeP8@db.ahlkrhnrkzxoxnrmnwjw.supabase.co:5432/postgres'
+export DATABASE_URL='postgresql://postgres:G9%23fL7mZ2%21qXeP8@db.ahlkrhnrkzxoxnrmnwjw.supabase.co:5432/postgres'
 npx prisma migrate deploy
+npx prisma db push
 npx prisma generate
